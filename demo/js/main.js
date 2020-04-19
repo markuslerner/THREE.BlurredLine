5566f9e4aed01cd567822a91fa41a1481fce8beb
Markus Lerner <m@markuslerner.com>
2020-04-19 14:22:34 +0200

    Move shader to main.js

--- demo/js/main.js
+++ demo/js/main.js
@@ -241,8 +241,48 @@ function createLines() {
       // time: { value: 1.0 },
       opacity: { value: params.opacity },
     },
-    vertexShader: document.getElementById( 'vertexShader' ).textContent,
-    fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+    vertexShader: `
+      precision mediump float;
+      precision mediump int;
+
+      uniform mat4 modelViewMatrix; // optional
+      uniform mat4 projectionMatrix; // optional
+
+      attribute vec3 position;
+      attribute vec4 color;
+
+      // varying vec3 vPosition;
+      varying vec4 vColor;
+
+      void main()	{
+
+        // vPosition = position;
+        vColor = color;
+
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+
+      }
+    `,
+    fragmentShader: `
+      precision mediump float;
+      precision mediump int;
+
+      uniform float time;
+      uniform float opacity;
+
+      // varying vec3 vPosition;
+      varying vec4 vColor;
+
+      void main()	{
+
+        vec4 color = vec4( vColor );
+        color.a *= opacity;
+        // color.r += sin( vPosition.x * 10.0 + time ) * 0.5;
+
+        gl_FragColor = color;
+
+      }
+    `,
     side: THREE.DoubleSide,
     transparent: true,
     depthTest: false,
